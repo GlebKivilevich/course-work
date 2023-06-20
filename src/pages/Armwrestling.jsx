8626882b8d9a351821f4d0c240de5d645178b033
Gleb Kivilevich <gleb1106f@gmail.com>
2023-06-20 19:48:53 +0400

    add armwrestling page, fix time table(gym)

--- src/pages/Armwrestling.jsx
+++ src/pages/Armwrestling.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import InfoBlockArm from "../componnets/Club/Armwrestling/InfoBlock/InfoBlockArm";
+import TimeTableArm from "../componnets/Club/Armwrestling/Timetable/TimeTableArm";
+import ContactArm from "../componnets/Club/Armwrestling/Contact/ContactArm";
 
 const Armwrestling = () => {
-  return <div>Armwrestling</div>;
+  return (
+    <div>
+      <InfoBlockArm />
+      <TimeTableArm />
+      <ContactArm />
+    </div>
+  );
 };
 
 export default Armwrestling;
