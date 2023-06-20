097fb95ddc7d0968dd6bdabd60464fa6046dd7a1
Gleb Kivilevich <gleb1106f@gmail.com>
2023-06-20 20:35:39 +0400

    fix table(rowing)

--- src/componnets/Club/Rowing/Timetable/TimetableRowing.jsx
+++ src/componnets/Club/Rowing/Timetable/TimetableRowing.jsx
@@ -13,38 +13,38 @@ const TimetableRowing = () => {
             {/* <!-- DATE --> */}
             <div class="col-data-rowing">
               <div class="cel-nodata-rowing"></div>
-              <div class="cel-data-rowing">ПН</div>
-              <div class="cel-data-rowing">ПТ</div>
+              <div class="cel-data-basketball">ПН</div>
+              <div class="cel-data-basketball">ПТ</div>
             </div>
             {/* <!-- ВРЕМЯ --> */}
             <div class="col-rowing">
               <div class="cel-time-header-rowing">ВРЕМЯ</div>
-              <div class="cel-time-rowing">
+              <div class="cel-time-basketball">
                 <div class="time-info-rowing">15:40 - 17:10</div>
               </div>
 
-              <div class="cel-time-rowing">
+              <div class="cel-time-basketball">
                 <div class="time-info-rowing">17:10 - 18:30</div>
               </div>
             </div>
             {/* <!--ГРУППА  --> */}
             <div class="col-rowing">
               <div class="cel-group-header-rowing">ГРУППА</div>
-              <div class="cel-group-rowing">
+              <div class="cel-group-basketball">
                 <div class="group-info-rowing">Общая</div>
               </div>
-              <div class="cel-group-rowing">
+              <div class="cel-group-basketball">
                 <div class="group-info-rowing">Общая</div>
               </div>
             </div>
             {/* МЕСТО ПРОВЕДЕНИЯ */}
             <div class="col-rowing">
               <div class="cel-place-header-rowing">МЕСТО ПРОВЕДЕНИЯ</div>
-              <div class="cel-place-rowing">
+              <div class="cel-place-basketball">
                 <div class="place-info-rowing">Тренажерный зал</div>
               </div>
 
-              <div class="cel-place-rowing">
+              <div class="cel-place-basketball">
                 <div class="place-info-rowing">Тренажерный зал</div>
               </div>
             </div>
