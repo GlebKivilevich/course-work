1c6c0418c4f699726ad7feb1ea47f2ff693b9286
Gleb Kivilevich <gleb1106f@gmail.com>
2023-06-20 19:26:52 +0400

    add basketball page

--- src/pages/Basketball.jsx
+++ src/pages/Basketball.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import InfoBlockBask from '../componnets/Club/Basketball/Infoblock/InfoBlockBask'
+import TimeTableBask from '../componnets/Club/Basketball/Timetable/TimeTableBask'
+import ContactBask from '../componnets/Club/Basketball/Contact/ContactBask'
 
 const Basketball = () => {
   return (
-    <div>Basketball</div>
+    <div>
+      <InfoBlockBask/>
+      <TimeTableBask/>
+      <ContactBask/>
+    </div>
   )
 }
 
